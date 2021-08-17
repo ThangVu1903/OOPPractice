429b42e4a9afe74bd6ed7795d37c065d8a357123
ThangVu1903 <vuthang19032002az@gmail.com>
2021-08-17 20:37:15 +0700

    first

---
+++ src/ooplesson7/Dog.java
@@ -0,0 +1,15 @@
+package ooplesson7;
+
+public class Dog {
+    public static void main(String[] args) {
+            String name;
+            int age;
+            String color;
+    }
+    public static void goForWalk(){
+
+    }
+    public static void eating(){
+
+    }
+}
